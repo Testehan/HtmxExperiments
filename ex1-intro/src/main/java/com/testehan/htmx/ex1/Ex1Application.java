38d84cc0101a22052ba47fdb15c6ea8f6730e9c6
DanTestehan <tdan89@yahoo.com>
2024-06-09 15:23:09 +0300

    security config and main class

---
+++ ex1-intro/src/main/java/com/testehan/htmx/ex1/Ex1Application.java
@@ -0,0 +1,18 @@
+package com.testehan.htmx.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+
+
+*/
+
+@SpringBootApplication
+public class Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1Application.class, args);
+	}
+
+}
---
+++ ex1-intro/src/main/java/com/testehan/htmx/ex1/security/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.testehan.htmx.ex1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/style.css").permitAll()
+                        .requestMatchers("/htmx-csrf.js").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .build();
+    }
+
+}
